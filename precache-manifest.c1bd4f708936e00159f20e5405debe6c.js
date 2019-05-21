906f7538db6fe2409fec4fce06ce1dba0a7041e2
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-05-21 15:15:18 +0100

    Updates

---
+++ precache-manifest.c1bd4f708936e00159f20e5405debe6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9d2df59566afc5382a95f302448d62",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "05bc50cbea168a86174a",
+    "url": "/react-movie-app/static/css/main.d4c15c3a.chunk.css"
+  },
+  {
+    "revision": "b5bf8e9a6f60c7fe2c2e",
+    "url": "/react-movie-app/static/js/2.a9bf33ee.chunk.js"
+  },
+  {
+    "revision": "05bc50cbea168a86174a",
+    "url": "/react-movie-app/static/js/main.75a42b83.chunk.js"
+  },
+  {
+    "revision": "e0c3c7f83a8b3d0de128",
+    "url": "/react-movie-app/static/js/runtime~main.4dfe89d3.js"
+  }
+]);
